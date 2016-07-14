3c896482d61bcbd500acf14c2ede3781dfd569f9
Calvin <calvinjin.tmx@gmail.com>
2016-07-14 11:11:46 -0400

    A sample js file to try grunt

---
+++ src/js/try-grunt.js
@@ -0,0 +1,15 @@
+(function(){
+	var a = "aadfdkbaje";
+	var map = {};
+	var tempArr = a.split("");
+
+	tempArr.forEach(function(e){
+		if( map.hasOwnProperty(e) ){
+			map[e]++;	
+		} else {
+			map[e] = 1;
+		}
+	});
+
+	console.log(map);
+})()
